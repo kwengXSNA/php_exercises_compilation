658b1ee2fcbf329b068a5669166340e030c67b80
kwengXSNA <112536564+kwengXSNA@users.noreply.github.com>
2022-10-17 19:56:37 -0700

    Create numbtwo.php

---
+++ numbtwo.php
@@ -0,0 +1,34 @@
+<?php
+
+class Str1ng{
+    // a
+    public ?string $str;
+
+    
+    function __construct($str){
+        $this->str = $str;
+    }
+
+    // b
+    public function NumberofChars(){
+        $NumberofChars = strlen($this->str);
+        return $NumberofChars;
+    }
+
+    //c
+    public function RemoveSpecialChars(){
+        $RemoveSpecialChars = str_replace(array('\'','"',',',';','<','>'),' ',$this->str);
+        return $RemoveSpecialChars;
+    }
+
+    //d
+    public function GroupStrInto(){
+        
+    }
+
+}
+
+
+
+
+?>
