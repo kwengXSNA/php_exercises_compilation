d7020a95fce9657f11fd9a852a893d15e1fa5ace
PNP-ACER06\Student.Admin <21103874@usc.edu.ph>
2022-10-19 10:34:14 +0800

    calc done taxcalc next, lablab... aja!

--- php_midtermexam_Calculator_mangapuro.php
+++ php_midtermexam_Calculator_mangapuro.php
@@ -1,9 +1,8 @@
 <?php
-$MOD = 1000000007;
-$SIZE = 10000;
 class Calculator{
 
     //a
+    //a property that accepts an array of numbers
     public $number_list = array();
 
     function __construct($number_list){
@@ -11,12 +10,14 @@ class Calculator{
     }
 
     //b
+    //a method that adds the numbers inside the number_list
     public function Add(){
         $sumOfArray = array_sum($this->number_list);
         return $sumOfArray;   
     }
 
     //c
+    //Subtract – a method that subtracts the first two numbers inside the number_list property 
     public function Subtract(){
         $diffOfArray = $this->number_list[0] - $this->number_list[1];
         rsort($this->number_list);
@@ -27,13 +28,14 @@ class Calculator{
     }
 
     //d
+    //a method that adds the numbers inside the number_list property and divides it by the number of items inside the number_list 
     public function Divide(){
         $avrgOfArray = array_sum($this->number_list)/count($this->number_list);
         return $avrgOfArray;
     }
 
     //e
-    //returning factorial of every element
+    //a method that returns the factorial of each number inside the number_list
     public function Factorial($fact, $number_list, $n){
         $fact = 0;
         for($i = 0; $i < $n; $i++){
@@ -41,12 +43,31 @@ class Calculator{
         }
     }
 
-    public function Prime(){
+    //f
+    //a method that returns an array of all prime numbers inside the number_list If no prime numbers are found, the method will return an empty array
+    function Prime(){
+        if ($this->number_list <= 1)
+            return false;
         
+        for ($i = 2; $i < $this->number_list; $i++)
+        if ($this->number_list % $i == 0)
+            return false;
+            
+        return true;
     }
+    
+    function returnPrime($number_list){
+        
+        for ($i = 2; $i <= $this->number_list; $i++){
+            if (Prime($i))
+            return $i;
+
+            else
+            return $number_list;
+        }
+    }
+ 
 
 }
-
-
 
 ?>
--- trial.php
+++ trial.php
@@ -1,32 +1,29 @@
 <?php
-$MOD = 1000000007 ;
-$SIZE = 10000 ;
- 
-function factorial($fact)
-{
-    $fact[0] = 1;
-    for ($i = 1; $i <= $GLOBALS['SIZE']; $i++)
-    {
- 
-        //calculating factorial
-        $fact[$i] = ($fact[$i - 1] * $i) % $GLOBALS['MOD'];
-    }
-    return $fact;
-}
- 
-//returning factorial of every element
-function ReturnFactorial($fact, $arr, $n)
-{
-    for($i = 0; $i < $n; $i++)
-    {
-        return $fact[$arr[$i]];
-    }
-}
 
-    $fact = array_fill(0,$SIZE + 1, 0);
+function Prime($number_list)
+{
+
+    if ($thisnumber_list <= 1)
+        return false;
  
-    $arr = array(3, 10, 200, 20, 12);
-    $n = count($arr);
-    $fact = factorial($fact);
-    ReturnFactorial($fact,$arr,$n);
+    for ($i = 2; $i < $thisnumber_list; $i++)
+        if ($thisnumber_list % $i == 0)
+            return false;
+ 
+    return true;
+}
+ 
+function returnPrime($number_list)
+{
+    for ($i = 2; $i <= $thisnumber_list; $i++)
+    {
+        if (Prime($i))
+            return $i;
+    }
+}
+ 
+
+$number_list = array();
+
+
 ?>
